ebfecb76e6b4b1e757bcbf80d1c4b96aec6f3af7
daffsri <daffodilsrikan@gmail.com>
2019-05-22 15:43:09 -0400

    add forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FindProfileComponent } from './find-profile/find-profile.component';
-import { DemoComponent } from './demo/demo.component';
-import { MenuComponent } from './menu/menu.component';
-import { SalesPlanComponent } from './sales-plan/sales-plan.component';
-import { SalesFormComponent } from './sales-form/sales-form.component';
-import { BillComponent } from './bill/bill.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FindProfileComponent } from "./find-profile/find-profile.component";
+import { DemoComponent } from "./demo/demo.component";
+import { MenuComponent } from "./menu/menu.component";
+import { SalesPlanComponent } from "./sales-plan/sales-plan.component";
+import { SalesFormComponent } from "./sales-form/sales-form.component";
+import { BillComponent } from "./bill/bill.component";
 
 @NgModule({
   declarations: [
@@ -21,14 +20,9 @@ import { BillComponent } from './bill/bill.component';
     SalesPlanComponent,
     SalesFormComponent,
     BillComponent
-    
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
